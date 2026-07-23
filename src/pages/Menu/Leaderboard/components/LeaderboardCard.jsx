8a243ea21f087c884651e82acee698cac0615d05
Romà <ro.oliver.gil@gmail.com>
2026-07-23 13:23:58 +0200

    feat(leaderboard): conditionally render position and rank styles based on game score

--- src/pages/Menu/Leaderboard/components/LeaderboardCard.jsx
+++ src/pages/Menu/Leaderboard/components/LeaderboardCard.jsx
@@ -2,26 +2,24 @@ import React from 'react'
 
 const LeaderboardCard = ({
   nickname,
-  key,
   game,
   lastGameDate,
   leaderboardPosition
 }) => {
   return (
-    <div key={key} className='leaderboard-card'>
+    <div className='leaderboard-card'>
       <div className='leaderboard-card-info'>
         <div
-          className={`leaderboard-card-position position-${leaderboardPosition}`}
+          className={`leaderboard-card-position ${game.score > 0 ? `position-${leaderboardPosition}` : ''}`}
         >
           <img
             src={game.icon}
             alt={`${game.name} icon`}
             className='leaderboard-card-icon'
           />
-          <span>{leaderboardPosition}</span>
+          <span>{game.score > 0 ? leaderboardPosition : ''}</span>
         </div>
         <div className='leaderboard-card-titles'>
-          {' '}
           <h4>{game.name}</h4>
           <span>{nickname}</span>
         </div>
@@ -30,7 +28,7 @@ const LeaderboardCard = ({
         <span>{game.score}</span>
         <div className='leaderboard-card-lastGame-container'>
           <p>Last game</p>
-          <span>{lastGameDate}</span>{' '}
+          <span>{lastGameDate}</span>
         </div>
       </div>
     </div>
